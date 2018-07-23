87613ba29c50a31afe69dd19beee16cf1922d53a
sid2207 <sid2207@gmail.com>
2018-07-23 17:51:55 +0530

    Initial commit

---
+++ app/src/main/java/askiitians_assignment/testing/com/example/android/askiitians_assignment/MainActivity.java
@@ -0,0 +1,18 @@
+package askiitians_assignment.testing.com.example.android.askiitians_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+
+    }
+}
